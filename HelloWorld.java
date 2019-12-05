58636511b954c95a2aac06a9b93e927a83cbd5f7
nari nari <narayana1994>
2019-12-05 17:32:39 +0530

    building

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World"); 
+        System.out.println("Hello, World");
+	system.out.println("hello devops");
     } 
 } 
