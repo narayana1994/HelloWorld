b0a91f244d5ea94ec91f931f8f9807e7ea9fb7fd
nari nari <narayana1994>
2019-12-05 17:43:09 +0530

    automation

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World");
-	system.out.println("hello devops");
+	system.out.println("hello devop");
+	
     } 
 } 
