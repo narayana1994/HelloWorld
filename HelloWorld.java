f242d935cc914be8d186746bc368aeb9e2afa539
nari nari <narayana1994>
2019-12-05 18:44:59 +0530

    f

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World");
-	system.out.println("hello, devopss");
+	system.out.println("Hello, devopss");
 	
     } 
 } 
